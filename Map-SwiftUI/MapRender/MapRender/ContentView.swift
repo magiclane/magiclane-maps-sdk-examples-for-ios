299a9308c92fb6f6a5f01310c9e7713b30a053b2
MagicLane Developer <magiclane-developer@users.noreply.github.com>
2025-11-19 17:20:11 +0200

    Update demo apps for SDK v2.1.3

--- Map-SwiftUI/MapRender/MapRender/ContentView.swift
+++ Map-SwiftUI/MapRender/MapRender/ContentView.swift
@@ -7,38 +7,25 @@ import SwiftUI
 import GEMKit
 
 struct ContentView: View {
-    @State private var zoom = 54
-    @State private var isRendering = true
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var isAppActive = true
     var body: some View {
-        NavigationStack {
-            MapReader { proxy in
-                MapBase()
-                    .mapRender(isRendering)
-                    .mapUserInteraction(isRendering)
-                    .onAppear() {
-                        goToPosition(proxy)
-                        // enableMapLabelsContinuousRendering(proxy)
-                    }
-                    .ignoresSafeArea(.all, edges: .bottom)
-                    .navigationTitle("GEMKit - SwiftUI")
-                    .navigationBarTitleDisplayMode(.inline)
-                    .toolbar {
-                        ToolbarItem(placement: .topBarTrailing) {
-                            Button(isRendering ? "RenderOff" : "RenderOn") {
-                                isRendering.toggle()
-                            }
-                        }
-                    }
+        MapReader { proxy in
+            MapBase(initialPosition: .amsterdam, initialZoomLevel: 54)
+                .mapCompass(false)
+                .mapRender(isAppActive)
+        }
+        .ignoresSafeArea()
+        .onChange(of: scenePhase) { newPhase in
+            switch newPhase {
+            case .active:
+                isAppActive = true
+            case .background:
+                isAppActive = false
+            default:
+                break
             }
         }
-    }
-    
-    func goToPosition(_ proxy: MapProxy) {
-        proxy.centerOn(coordinates: .amsterdam, zoomLevel: zoom)
-    }
-    
-    func enableMapLabelsContinuousRendering(_ proxy: MapProxy) {
-        proxy.mapViewController?.getPreferences().setMapLabelsContinuousRendering(true)
     }
 }
 
